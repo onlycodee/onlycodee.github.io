6a715b7c92fc6d025c2e76ac64c9bde8a565fc0c
Vũ Phương <vuvietphuonggm@gmail.com>
2021-07-29 14:28:40 +0700

    add function to button

--- js/changeImage.js
+++ js/changeImage.js
@@ -1,8 +1,47 @@
-var imagePath = "img/products/obagi/"
+var imagePath = "img/products/"
 $(document).ready(function () {
-    console.log("Ready");
-    $("#obagi-1").on("click", function() {
-        console.log("obagi click");
-        $("#product-list").attr('src', imagePath + "1.png");
-    });
+    // $("#obagi-1").on("click", function() {
+    //     console.log("obagi click");
+    //     $("#product-list").attr('src', imagePath + "1.png");
+    // });
+    var cnt = 1;
+    for (var i = 0; i < productNames.length; i++) {
+        var compo = `<div class="panel panel-default">\
+                        <div class="panel-heading">\
+                            <h4 class="panel-title">\
+                                <a data-toggle="collapse" data-parent="#accordian" href="#${productNames[i].slice(0, 2)}">\
+                                    <span class="badge pull-right"><i class="fa fa-plus"></i></span>\
+                                    ${productNames[i]}\
+                                </a>\
+                            </h4>\
+                        </div>
+                        <div id="${productNames[i].slice(0, 2)}" class="panel-collapse collapse">\
+                            <div class="panel-body">\
+                                <ul>`;
+        for (var j = 0; j < functions[i].length; j++) {
+            compo += `<li id="${cnt++}" class="btn">${functions[i][j]}</li>`;
+        }
+        compo += `</ul></div></div></div>`;
+                    //             </ul>\
+                    //         </div>\
+                    //     </div>\
+                    // </div>';
+
+        $("#accordian").append(compo);
+    }
+    cnt = 1;
+    //console.log(productNames.length);
+    for (var i = 0; i < productNames.length; i++) {
+        var name = productNames[i].slice(0, 2).toLowerCase();
+        for (var j = 0; j < functions[i].length; j++) {
+            var curIndex = j + 1;
+            (function(name, curIndex){
+                $(`#${cnt}`).on("click", function() {
+                    console.log('name: ' + name + ' curindex: ' + curIndex);
+                    $("#product-list").attr('src', imagePath + name + `/${curIndex}.png`);
+                });
+            })(name, curIndex);
+            cnt++;
+        }
+    }
 });
---
+++ js/data.js
@@ -0,0 +1,65 @@
+var productNames = ["OBAGI - ELASTIDERM", "MARTIDERM", "DERMAFIRM", "GOODNDOC", "LA ROCHE-POSAY", "EUCERIN"];
+var functions = [
+    [
+    "NGĂN NGỪA LÃO HÓA",
+    "ĐẶC TRỊ NÁM",
+    "TRỊ NÁM + VITAMIN C",
+    "TÁI TẠO TRẺ HÓA",
+    "CHỐNG NẮNG",
+    "TRỊ MỤN DA DẦU Cấp ẨM",
+    "ĐỔI MỚI TẾ BÀO",
+    "CHĂM SÓC MẮT",
+    "TRẮNG SÁNG DA",
+    "CHO DA NHẠY CẢM"
+    ],
+    [
+        "LÀM SẠCH",
+        "PHỤC HỒI MÀNG DA",
+        "TRỊ MỤN , SE NHỎ CHÂN LÔNG",
+        "CHĂM SÓC CƠ THỂ",
+        "KHÔI PHỤC ĐỘ ẨM",
+        "CHỐNG OXY HÓA",
+        "TÁI TẠO TRẺ HÓA DA",
+        "ĐIỀU TRỊ SẮC TỐ",
+        "CHỐNG LÃO HÓA CHUYÊN SÂU"
+    ],
+    [
+        "TẨY TRANG - DA CHẾT",
+        "LÀM SẠCH",
+        "NƯỚC CÂN BẰNG",
+        "TINH CHẤT",
+        "SỮA DƯỠNG",
+        "KEM DƯỠNG",
+        "KEM CHỐNG NẮNG-KEM NỀN",
+        "MẶT NẠ MIẾNG",
+        "AMPOULE",
+        "NÂNG TONE BODY",
+        "MINI KIT",
+        "DOCTOR CARE"
+    ],
+    [
+        "TẤT CẢ"
+    ],
+    [
+        "NƯỚC KHOÁNG BẢO VỆ DA",
+        "TẨY TRANG - LÀM SACH SÂU",
+        "CHĂM SÓC DA DẦU MỤN",
+        "CHĂM SÓC DA NHẠY CẢM",
+        "TRỊ MẨN ĐỔ VIÊM NGỨA",
+        "BẢO VỆ DA CHỐNG NẮNG 1",
+        "BẢO VỆ DA CHỐNG NẮNG 2",
+        "HỒI PHỤC LÀN DA , DỊU KÍCH ỨNG",
+        "LÀM SÁNG ĐỀU MÀU DA",
+        "HYALU B5 , PIGMENTCLAR"
+    ],
+    [
+        "DA NHỜN MỤN",
+        "DA MẤT NƯỚC",
+        "SÁNG DA THÂM NÁM",
+        "CHỐNG NẮNG",
+        "DÀNH CHO DA LÃO HÓA",
+        "DÀNH CHO DA NHẠY CẢM",
+        "DA CHÀM, LÁC SỮA, RÔM SẨY",
+        "CƠ THỂ - TÓC"
+    ]
+]
